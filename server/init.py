0877d886d1189d8a9d1925afd14b8a26787c2ae0
Amar <amaar.ahmednur@student.moringaschool.com>
2024-09-28 09:43:08 +0300

    initialized the database

--- server/init.py
+++ server/init.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from models import db
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'  # Change to your preferred DB
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config.from_pyfile('instance/config.py')
 
     db.init_app(app)
 
-    # Import the routes
+    # Initialized Flask-Migrate
+    migrate = Migrate(app, db)
+
+    # Imported the routes
     from routes import main
     app.register_blueprint(main)
 
